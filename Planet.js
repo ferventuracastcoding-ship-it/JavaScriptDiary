daefc5965d2a34c2b680949c79759b39bae49168
ferventuracastcoding-ship-it <ferventuracastcoding@gmail.com>
2026-08-10 16:15:10 -0700

    Add logic for Mars and Earth comparison
    Fix syntax errors and improve logic in Planet.js.

---
+++ Planet.js
@@ -0,0 +1,10 @@
+const mars = 1;
+forEach(mars < 10){
+  console.log("GO FOR TOUR DREAMS!");
+  return 0;
+  if(mars > earth){
+    console.log("Planet year");
+  } else (mars < earth) {
+    console.log("ERROR!");
+  }
+}
