4de7e7393bb64e1e6fbfae0bf60cfc65e3c6aa88
ferventuracastcoding-ship-it <ferventuracastcoding@gmail.com>
2026-08-10 16:16:28 -0700

    Update Planet.js

--- Planet.js
+++ Planet.js
@@ -1,10 +1,11 @@
 const mars = 1;
 forEach(mars < 10){
-  console.log("GO FOR TOUR DREAMS!");
+  console.log("GO FOR YOUR DREAMS!");
   return 0;
   if(mars > earth){
     console.log("Planet year");
   } else (mars < earth) {
     console.log("ERROR!");
+    console.log("New heavenly kingdom?");
   }
 }
